ba993e40cd4279c15e7e9d6ec0b27bdffa81cb4b
meimeic <zhao.jian.ning66@gmail.com>
2016-03-30 22:32:44 +0800

    12345

--- XYS.Report.WS/ReportStatus.asmx.cs
+++ XYS.Report.WS/ReportStatus.asmx.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Services;
 
@@ -14,7 +12,7 @@ namespace XYS.Report.WS
     //[System.ComponentModel.ToolboxItem(false)]
     // 若要允许使用 ASP.NET AJAX 从脚本中调用此 Web 服务，请取消注释以下行。 
     // [System.Web.Script.Services.ScriptService]
-    public class ReportStatus : System.Web.Services.WebService
+    public class ReportStatus : WebService
     {
 
         [WebMethod]
