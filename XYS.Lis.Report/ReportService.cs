fd37189efce560a1dc19dfc7c6d1e868a63d8682
meimeic <zhao.jian.ning66@gmail.com>
2016-07-18 15:21:07 +0800

    we

--- XYS.Lis.Report/ReportService.cs
+++ XYS.Lis.Report/ReportService.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 
@@ -7,26 +8,65 @@ using log4net;
 
 using XYS.Util;
 using XYS.Report;
+using XYS.Lis.Report.Util;
 using XYS.Lis.Report.Model;
 using XYS.Lis.Report.Persistent;
 namespace XYS.Lis.Report
 {
+    public delegate void HandleErrorHandler(LabReport report);
+    public delegate void HandleSuccessHandler(LabReport report);
     public class ReportService
     {
         #region 静态变量
-        private static ReportPKDAL PKDAL;
-        private static ILog LOG = LogManager.GetLogger("LisReportHandle");
+        private static ILog LOG;
+        private static int WorkerCount;
+        private static readonly Hashtable Type2HandleMap;
+        private static readonly Hashtable Section2FillTypeMap;
+        private static readonly ReportService ServiceInstance;
+
+        private readonly ReportPKDAL PKDAL;
+        private readonly BlockingCollection<LabReport> InitRequestQueue;
+        #endregion
+
+        #region 私有事件
+        private event HandleErrorHandler m_handleErrorEvent;
+        private event HandleSuccessHandler m_handleCompleteEvent;
         #endregion
 
         #region 构造函数
         static ReportService()
         {
-            //初始化handleTable
-            PKDAL = new ReportPKDAL();
+            WorkerCount = 10;
+            Type2HandleMap = new Hashtable(5);
+            Section2FillTypeMap = new Hashtable(20);
+            ServiceInstance = new ReportService();
+            LOG = LogManager.GetLogger("LisReportHandle");
         }
-        public ReportService()
+        private ReportService()
         {
+            this.PKDAL = new ReportPKDAL();
+            this.InitRequestQueue = new BlockingCollection<LabReport>(1000);
             this.Init();
+        }
+        #endregion
+
+        #region 静态属性
+        public static ReportService LabService
+        {
+            get { return ServiceInstance; }
+        }
+        #endregion
+        
+        #region 事件属性
+        public event HandleErrorHandler HandleErrorEvent
+        {
+            add { this.m_handleErrorEvent += value; }
+            remove { this.m_handleErrorEvent -= value; }
+        }
+        public event HandleSuccessHandler HandleCompleteEvent
+        {
+            add { this.m_handleCompleteEvent += value; }
+            remove { this.m_handleCompleteEvent -= value; }
         }
         #endregion
 
@@ -41,31 +81,105 @@ namespace XYS.Lis.Report
         }
         #endregion
 
+        #region 生产者方法
+        public void InitReport(LabReport report)
+        {
+            LOG.Info("将处理报告请求加入到处理请求队列");
+            this.InitRequestQueue.Add(report);
+        }
+        #endregion
+
+        #region 消费者方法
+        protected void InitRequestConsumer()
+        {
+            foreach (LabReport report in this.InitRequestQueue.GetConsumingEnumerable())
+            {
+                LOG.Info("报告处理线程开始处理报告,报告ID为:" + report.ReportPK.ID);
+                this.InnerHandle(report);
+            }
+        }
+        #endregion
+
         #region 辅助方法
+        protected List<Type> GetFillTypes(ReportPK RK)
+        {
+            return Section2FillTypeMap[RK.SectionNo] as List<Type>;
+        }
+        protected IHandle GetHandle(Type type)
+        {
+            return Type2HandleMap[type] as IHandle;
+        }
+
+        private static void InitFillTable()
+        {
+            lock (Section2FillTypeMap)
+            {
+                ConfigManager.InitSection2FillElementTable(Section2FillTypeMap);
+            }
+        }
+        private static void InitHandleTable()
+        {
+            lock (Type2HandleMap)
+            {
+                ConfigManager.InitType2HandleTable(Type2HandleMap);
+            }
+        }
         #endregion
 
         #region 触发事件
+        protected void OnError(LabReport report)
+        {
+            HandleErrorHandler handler = this.m_handleErrorEvent;
+            if (handler != null)
+            {
+                handler(report);
+            }
+        }
+        protected void OnSuccess(LabReport report)
+        {
+            HandleSuccessHandler handler = this.m_handleCompleteEvent;
+            if (handler != null)
+            {
+                handler(report);
+            }
+        }
         #endregion
 
         #region 私有方法
         private void Init()
         {
         }
-        private void LogError(ReportReportElement report)
+        private void InnerHandle(LabReport report)
         {
-            LOG.Error(report.HandleResult.Message, report.HandleResult.Exception);
-        }
-        #endregion
-
-        #region 
-        protected void HandleReport(LabReport report)
-        {
-            if (report.ReportPK == null)
+            bool result = false;
+            IHandle handle = null;
+            ReportPK RK = report.ReportPK;
+            List<Type> lt = GetFillTypes(RK);
+            List<IFillElement> elements = null;
+            if (lt != null && lt.Count > 0)
             {
-                //错误
+                foreach (Type type in lt)
+                {
+                    handle = GetHandle(type);
+                    if (handle != null)
+                    {
+                        elements = new List<IFillElement>(10);
+                        result = handle.InitElement(elements, RK, type);
+                        if (!result)
+                        {
+                            break;
+                        }
+                        report.ItemTable.Add(type.Name, elements);
+                    }
+                }
+            }
+            if (result)
+            {
+                OnSuccess(report);
             }
             else
             {
+                OnError(report);
             }
         }
         #endregion
